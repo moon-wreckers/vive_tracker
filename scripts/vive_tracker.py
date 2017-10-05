346583aaef17a59373db1de19bdf5303c60f423e
Daniel Arnett <arnett.daniel@gmail.com>
2017-10-05 16:16:16 -0400

    Trying to get Quaternion working. Also printing Euler Pose.

--- scripts/vive_tracker.py
+++ scripts/vive_tracker.py
@@ -12,23 +12,19 @@ v.print_discovered_objects()
 def vive_tracker():
     rospy.init_node('vive_tracker_frame')
     br = tf.TransformBroadcaster()
-    pub = rospy.Publisher('vive_tracker_str', String, queue_size=10)
+    pub = rospy.Publisher('vive_tracker_euler', String, queue_size=10)
     #rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(30) # 10hz
     while not rospy.is_shutdown():
         #hello_str = "hello world %s" % rospy.get_time()
         txt = ""
-        tfVals = [None] * 7
-        for i, each in enumerate(v.devices["tracker_1"].get_pose_quaternion()):
+        for i, each in enumerate(v.devices["tracker_1"].get_pose_euler()):
             txt += "%.4f" % each
             txt += " "
-            tfVals[i] = each
-            print(tfVals[i])
-        print("")
+        #print("")
         #print("\r" + txt)
-        #rospy.loginfo(txt)
-        br.sendTransform((tfVals[0],tfVals[1],tfVals[2]),
-                         (tfVals[3], tfVals[4], tfVals[5], tfVals[6]),
+        rospy.loginfo(txt)
+        br.sendTransform((v.devices["tracker_1"].get_pose_quaternion()),
                          rospy.Time.now(),
                          "vive_tracker",
                          "map")
