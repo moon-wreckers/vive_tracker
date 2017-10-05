6d6eba7939f20934edddcc855cf9771340b28bfa
Daniel Arnett <arnett.daniel@gmail.com>
2017-10-05 03:42:17 -0400

    Now running in rviz! Just use `rosrun tf static_transform_publisher 0.0 0.0 0.0 0.0 0.0 0.0 map my_frame 100`, then start the node with `rosrun vive_tracker vive_tracker.py`, then open rviz, make an axis, and set the frame to vive tracker.

--- scripts/vive_tracker.py
+++ scripts/vive_tracker.py
@@ -4,22 +4,35 @@ from std_msgs.msg import String
 import triad_openvr
 import time
 import sys
+import tf
 
 v = triad_openvr.triad_openvr()
 v.print_discovered_objects()
 
 def vive_tracker():
-    pub = rospy.Publisher('vive_tracker', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('vive_tracker_frame')
+    br = tf.TransformBroadcaster()
+    pub = rospy.Publisher('vive_tracker_str', String, queue_size=10)
+    #rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(30) # 10hz
     while not rospy.is_shutdown():
         #hello_str = "hello world %s" % rospy.get_time()
         txt = ""
-        for each in v.devices["tracker_1"].get_pose_euler():
+        tfVals = [None] * 7
+        for i, each in enumerate(v.devices["tracker_1"].get_pose_quaternion()):
             txt += "%.4f" % each
             txt += " "
+            tfVals[i] = each
+            print(tfVals[i])
+        print("")
         #print("\r" + txt)
-        rospy.loginfo(txt)
+        #rospy.loginfo(txt)
+        br.sendTransform((tfVals[0],tfVals[1],tfVals[2]),
+                         (tfVals[3], tfVals[4], tfVals[5], tfVals[6]),
+                         rospy.Time.now(),
+                         "vive_tracker",
+                         "map")
+
         pub.publish(txt)
         rate.sleep()
 
