Odometry publishing! Next try to fix the wraparound issue, maybe try delta_q = q1 * inverse(q2) to get the difference of the two orientation quaternions.

scripts/vive_tracker.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 import triad_openvr
 import time
 import sys
 import tf
 import numpy as np
+import math
 
 print('\n')
 try:
@@ -29,6 +32,8 @@ def vive_tracker():
     rospy.init_node('vive_tracker_frame')
     broadcaster = { }
     publisher = { }
+    last_time = { }
+    last_pose = { }
     rate = rospy.Rate(30) # 10hz
     while not rospy.is_shutdown():
         # For each Vive Device
@@ -49,9 +54,44 @@ def vive_tracker():
             [x,y,z,yaw,pitch,roll] = v.devices[deviceName].get_pose_euler()
             if deviceName not in publisher:
                 publisher[deviceName] = rospy.Publisher(deviceName, String, queue_size=10)
+                
             
             publisher[deviceName].publish('  X: ' + str(x) + '  Y: ' + str(y) + '  Z: ' + str(z) + '  Pitch: ' + str(pitch) + '  Roll: ' + str(roll) + '  Yaw: ' + str(yaw))
-
+            
+            if "reference" not in deviceName:
+                if deviceName + "_odom" not in publisher:
+                    publisher[deviceName + "_odom"] = rospy.Publisher(deviceName + "_odom", Odometry, queue_size=50)
+                # next, we'll publish the odometry message over ROS
+                odom = Odometry()
+                odom.header.stamp = time
+                odom.header.frame_id = "vive_world"
+    
+                # set the position
+                odom.pose.pose = Pose(Point(x, y, z), Quaternion(qx,qy,qz,qw))
+    
+                # set the velocity
+                odom.child_frame_id = "vive_world"
+                if deviceName in last_time and deviceName in last_pose:
+                    t = time - last_time[deviceName]
+                    vx = (x - last_pose[deviceName].position.x) / t.to_sec()
+                    vy = (y - last_pose[deviceName].position.y) / t.to_sec()
+                    vz = (z - last_pose[deviceName].position.z) / t.to_sec()
+                    last_quat = last_pose[deviceName].orientation
+                    explicit_quat = [last_quat.x, last_quat.y, last_quat.z, last_quat.w]
+                    euler = tf.transformations.euler_from_quaternion(explicit_quat)
+                    last_roll = euler[0]
+                    last_pitch = euler[1]
+                    last_yaw = euler[2]
+#                    print("Last Roll: " + str(euler[0]) + " This Roll: " + str(math.radians(roll)))
+                    v_roll  = (math.radians(roll) - last_roll  ) / t.to_sec()
+                    v_pitch = (math.radians(pitch) - last_pitch) / t.to_sec()
+                    v_yaw   = (math.radians(yaw) - last_yaw    ) / t.to_sec()
+                    odom.twist.twist = Twist(Vector3(vx, vy, vz), Vector3(v_roll, v_pitch, v_yaw))
+    
+                # publish the message
+                publisher[deviceName + "_odom"].publish(odom)
+                last_time[deviceName] = time
+                last_pose[deviceName] = odom.pose.pose
         rate.sleep()
 
 